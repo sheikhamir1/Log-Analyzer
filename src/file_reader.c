f8dfa950bc98da4e77fe3411bd4307b6f0f65e86
Sheikh amir <Sheikhamir036@gmail.com>
2026-01-02 20:06:29 +0530

    Implement read_line and close_file functions in FileReader

--- src/file_reader.c
+++ src/file_reader.c
@@ -13,3 +13,15 @@ FileReader* open_file(const char *filename){
     }
     return reader;
 }
+
+char* read_line(FileReader *reader , char *line , size_t max_len){
+    if(!reader || !reader->file || !line) return NULL;
+    return fgets(line,max_len, reader->file);
+}
+
+void close_file(FileReader *reader){
+    if(reader){
+        if(reader->file) fclose(reader->file);
+        free(reader);
+    }
+}
